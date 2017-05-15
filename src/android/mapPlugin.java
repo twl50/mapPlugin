f45bba69406a93032d4a1e89a9407f0a96659753
tangwl <tangwl@doordu.com>
2017-05-15 17:21:30 +0800

    更改下插件方法  以及把add成功 后的demo上传

--- src/android/mapPlugin.java
+++ src/android/mapPlugin.java
@@ -14,14 +14,35 @@ public class mapPlugin extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
-            String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
-            return true;
-        }
+        if(args.length()<2){
+    		callbackContext.error("参数个数不对");
+    		return true;
+    	}else{
+    		if (action.equals("Plus")) {
+            	int p1 = args.getInt(0);
+            	int p2 = args.getInt(1);
+                this.plus(p1,p2,callbackContext);
+                return true;
+            }else if(action.equals("minus")){
+            	int p1 = args.getInt(0);
+            	int p2 = args.getInt(1);
+            	this.minus(p1,p2,callbackContext);
+            }
+    	}
         return false;
     }
 
+    private void minus(int pram1,int pram2,CallbackContext callbackContext){
+    	if(pram1-pram2>0){
+    		callbackContext.success("正确");
+    	}else{
+    		callbackContext.error("不正确");
+    	}
+    }
+    private void plus(int pram1,int pram2,CallbackContext callbackContext){
+    	int result= pram1+pram2;
+    	callbackContext.success("等于--"+result);
+    }
     private void coolMethod(String message, CallbackContext callbackContext) {
         if (message != null && message.length() > 0) {
             callbackContext.success(message);
